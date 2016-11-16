7b907f3951e903c3e3e4ea71184617ab51746dd2
sistemas <=>
2016-11-16 17:00:10 +0100

    Lets get back to root (will need to xhost+)

--- script/emulator.sh
+++ script/emulator.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 ldconfig -r /hostlib
-UNIXSOCKET=/tmp/.X11-unix/X${DISPLAY:1}
-UNIXSOCKET_OWNER=`stat -c %U $UNIXSOCKET`
-sudo chmod o+rw /dev/kvm
-sudo chown $USER $UNIXSOCKET
 emulator64-x86 @nexus
-sudo chmod o-rw /dev/kvm
-sudo chown $UNIXSOCKET_OWNER $UNIXSOCKET
